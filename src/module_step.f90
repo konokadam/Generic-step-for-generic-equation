16b248a32f2557d85c99ad50508490204e360a92
konokadam <53538563+konokadam@users.noreply.github.com>
2021-09-17 07:50:08 +0300

    Update module_step.f90

--- src/module_step.f90
+++ src/module_step.f90
@@ -17,10 +17,11 @@ module step_class
     end type derivative_abstract_type
 
     abstract interface
-        subroutine derivative_proc(self, h, x, xdot)
+        subroutine derivative_proc(self, t, h, x, xdot)
             import :: derivative_abstract_type, rp
             class(derivative_abstract_type), intent(inout) :: self
-            real(rp),intent(in) :: h                    !!!Step size
+            real(rp), intent(in) :: t
+            real(rp), intent(in) :: h                    !!!Step size
             real(rp), intent(in) :: x(:)                !!!Vector of the equation set
             real(rp), intent(out) :: xdot(size(x))      !!!Vector of the derivatives of equation set
         end subroutine
@@ -37,10 +38,11 @@ module step_class
     end type step_abstract_type
 
     abstract interface
-        subroutine step_proc(f, x, h, dx)
+        subroutine step_proc(f, t, x, h, dx)
             use :: precision, only: rp
             import :: derivative_abstract_type
             class(derivative_abstract_type) :: f        !!!The derivative function come through overloaded deferred procedure f%derivative(...)
+            real(rp), intent(in) :: t
             real(rp), intent(in) :: x(:)                !!!Vector of the equation set
             real(rp), intent(in) :: h                   !!!Step size
             real(rp), intent(out) :: dx(:)              !!!Vector of one step of the equation set
@@ -108,26 +110,28 @@ contains
     !!!Procedures for the steps
     
     !!!Procedure for simple Euler step
-    subroutine step_euler(f, x, h, dx)
+    subroutine step_euler(f, t, x, h, dx)
 
         implicit none
         class(derivative_abstract_type) :: f
+        real(rp), intent(in) :: t
         real(rp), intent(in) :: x(:)
         real(rp), intent(in) :: h
         real(rp), intent(out) :: dx(:)
 
         real(rp), dimension(size(x)) :: xdot
 
-        call f%derivative(h, x, xdot)
+        call f%derivative(t, h, x, xdot)
         dx = h * xdot
 
     end subroutine step_euler
 
     !!!Procedure for 4th order Runge-Kutta step
-    subroutine step_rk4(f, x, h, dx)
+    subroutine step_rk4(f, t, x, h, dx)
 
         implicit none
         class(derivative_abstract_type) :: f
+        real(rp), intent(in) :: t
         real(rp), intent(in) :: x(:)
         real(rp), intent(in) :: h
         real(rp), intent(out) :: dx(:)
@@ -137,10 +141,10 @@ contains
 
         ho2 = 0.5_rp * h
 
-        call f%derivative(0._rp, x, xdot1)           !!!1st RK4 step
-        call f%derivative(ho2, x+ho2*xdot1, xdot2)   !!!2nd RK4 step
-        call f%derivative(ho2, x+ho2*xdot2, xdot3)   !!!3rd RK4 step
-        call f%derivative(h, x+h*xdot3, xdot4)       !!!4th RK4 step
+        call f%derivative(t, 0._rp, x, xdot1)           !!!1st RK4 step
+        call f%derivative(t+ho2, ho2, x+ho2*xdot1, xdot2)   !!!2nd RK4 step
+        call f%derivative(t+ho2 + ho2, x+ho2*xdot2, xdot3)   !!!3rd RK4 step
+        call f%derivative(t+h, h, x+h*xdot3, xdot4)       !!!4th RK4 step
 
         dx = h * (xdot1 + 2._rp*xdot2 + 2._rp*xdot3 + xdot4)/6._rp
 
